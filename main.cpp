5eec0e83d1858c22a8584dc73a8db986650d267f
kamisatoMasatoshi <k021g1271@g.neec.ac.jp>
2024-09-07 17:44:08 +0900

    scene add

--- main.cpp
+++ main.cpp
@@ -9,6 +9,14 @@ const int WIN_WIDTH = 600;
 // ウィンドウ縦幅
 const int WIN_HEIGHT = 800;
 
+enum class Scene
+{
+	Title,//タイトル
+	Game,//ステージ
+};
+
+Scene scene_ = Scene::Title;
+
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
 	_In_ int nCmdShow) {
 	// ウィンドウモードに設定
@@ -59,7 +67,20 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		//---------  ここからプログラムを記述  ----------//
 
 		// 更新処理
+		switch (scene_)
+		{	
+		case Scene::Title:
+			
+			break;
 
+
+		case Scene::Game:
+			
+			break;
+		default:
+
+			break;
+		}
 
 		// 描画処理
 
